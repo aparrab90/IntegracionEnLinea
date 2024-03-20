6a3e62f8f8b9c6a75585d20154e32c0df5616e24
Angel Javier Parra Bagua <aparrab90@gmail.com>
2024-03-20 00:16:05 -0500

    Entorno para Integracion Fuera de Linea

--- app/sender.py
+++
@@ -1,9 +0,0 @@
-import pika
-credentials = pika.PlainCredentials('admin', 'admin')
-parameters = pika.ConnectionParameters(host='localhost',credentials=credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-channel.queue_declare(queue='saludos')
-channel.basic_publish(exchange='',routing_key='saludos',body="Hola Mundo")
-connection.close()
-
